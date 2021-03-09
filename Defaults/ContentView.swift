68b6d90d82820e3030aa41f1783959f5a1821a90
Praneet <praneetquantumx@gmail.com>
2021-03-09 18:53:25 +0530

    Initial Commit

---
+++ Defaults/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Defaults
+//
+//  Created by Praneet S on 09/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Defaults/DefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultsApp.swift
+//  Defaults
+//
+//  Created by Praneet S on 09/03/21.
+//
+
+import SwiftUI
+
+@main
+struct DefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
